31fa397f97f9fdba3bd3625e3e25adc5e4086065
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-05-27 18:11:00 +0100

    First Test

--- Measure_Sne.py
+++ Measure_Sne.py
@@ -9,8 +9,8 @@ GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
 GPIO.setup(23, GPIO.OUT)# set GPIO 25 as output for white led  
 GPIO.setup(24, GPIO.OUT)# set GPIO 24 as output for red led  
   
-white = GPIO.PWM(24,10000)    # create object white for PWM on port 24 at 100 Hertz  
-red = GPIO.PWM(23, 10000)      # create object red for PWM on port 23 at 100 Hertz  
+white = GPIO.PWM(24,100)    # create object white for PWM on port 24 at 100 Hertz  
+red = GPIO.PWM(23, 100)      # create object red for PWM on port 23 at 100 Hertz  
   
 white.start(0)              # start white led on 0 percent duty cycle (off)  
 red.start(0)              # red fully on (100%)  
@@ -31,7 +31,8 @@ for i in range(0,len(m),2):      # 101 because it stops when it finishes 100
     l1=[]
     for j in range(0,100):
     	l1.append(tsl.readLux(gain=16))
-    time_array.append(t[i]);luxarray.append(np.mean(l1))
+    print np.mean(l1)
+    time_array.append(t[i]);luxarray.append(np.sum(l1))
     sleep(pause_time) 
 
 plt.scatter(time_array, luxarray)
