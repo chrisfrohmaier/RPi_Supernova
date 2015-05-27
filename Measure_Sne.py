c7135e6a063b26a1d4f24e6dce95f89fc6ede909
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-05-27 17:21:13 +0100

    First Test

--- Measure_Sne.py
+++ Measure_Sne.py
@@ -8,8 +8,8 @@ GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
 GPIO.setup(23, GPIO.OUT)# set GPIO 25 as output for white led  
 GPIO.setup(24, GPIO.OUT)# set GPIO 24 as output for red led  
   
-white = GPIO.PWM(24,10000)    # create object white for PWM on port 24 at 100 Hertz  
-red = GPIO.PWM(23, 10000)      # create object red for PWM on port 23 at 100 Hertz  
+white = GPIO.PWM(24,100)    # create object white for PWM on port 24 at 100 Hertz  
+red = GPIO.PWM(23, 100)      # create object red for PWM on port 23 at 100 Hertz  
   
 white.start(0)              # start white led on 0 percent duty cycle (off)  
 red.start(0)              # red fully on (100%)  
@@ -17,7 +17,7 @@ red.start(0)              # red fully on (100%)
 # now the fun starts, we'll vary the duty cycle to   
 # dim/brighten the leds, so one is bright while the other is dim  
   
-pause_time = 0.1           # you can change this to slow down/speed up  
+pause_time = 0.2           # you can change this to slow down/speed up  
 t=np.load('Ia_Time.npy')
 m=np.load('Ia_Mag.npy')
 
