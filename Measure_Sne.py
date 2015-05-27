7b1097a1a7685e0f49fec9173aa1109b6ad490b3
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-05-27 17:41:39 +0100

    First Test

--- Measure_Sne.py
+++ Measure_Sne.py
@@ -9,8 +9,8 @@ GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
 GPIO.setup(23, GPIO.OUT)# set GPIO 25 as output for white led  
 GPIO.setup(24, GPIO.OUT)# set GPIO 24 as output for red led  
   
-white = GPIO.PWM(24,1000)    # create object white for PWM on port 24 at 100 Hertz  
-red = GPIO.PWM(23, 1000)      # create object red for PWM on port 23 at 100 Hertz  
+white = GPIO.PWM(24,10000)    # create object white for PWM on port 24 at 100 Hertz  
+red = GPIO.PWM(23, 10000)      # create object red for PWM on port 23 at 100 Hertz  
   
 white.start(0)              # start white led on 0 percent duty cycle (off)  
 red.start(0)              # red fully on (100%)  
