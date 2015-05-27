b975b664e49f5ba316d19b89366119026a2fe537
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-05-27 18:23:20 +0100

    First Test

--- Measure_Sne.py
+++ Measure_Sne.py
@@ -25,7 +25,7 @@ m=np.load('Ia_Mag.npy')
 tsl=TSL2561()
 time_array=[]
 luxarray=[]
-for i in range(0,len(m),2):      # 101 because it stops when it finishes 100  
+for i in range(0,len(m)):      # 101 because it stops when it finishes 100  
      
     red.ChangeDutyCycle(m[i])
     l1=[]
