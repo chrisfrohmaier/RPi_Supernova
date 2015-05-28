202ac90960160a0d7ac83c5afe50c726d3fc6fb7
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-05-28 10:59:22 +0100

    First Test

--- Measure_Sne.py
+++ Measure_Sne.py
@@ -6,11 +6,11 @@ from TSL2561 import TSL2561
 import matplotlib.pyplot as plt
 GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM  
   
-GPIO.setup(23, GPIO.OUT)# set GPIO 25 as output for white led  
-GPIO.setup(24, GPIO.OUT)# set GPIO 24 as output for red led  
+GPIO.setup(18, GPIO.OUT)# set GPIO 25 as output for white led  
+#GPIO.setup(24, GPIO.OUT)# set GPIO 24 as output for red led  
   
-white = GPIO.PWM(24,10000)    # create object white for PWM on port 24 at 100 Hertz  
-red = GPIO.PWM(23, 10000)      # create object red for PWM on port 23 at 100 Hertz  
+#white = GPIO.PWM(24,10000)    # create object white for PWM on port 24 at 100 Hertz  
+red = GPIO.PWM(18, 400)      # create object red for PWM on port 23 at 100 Hertz  
   
 white.start(0)              # start white led on 0 percent duty cycle (off)  
 red.start(0)              # red fully on (100%)  
