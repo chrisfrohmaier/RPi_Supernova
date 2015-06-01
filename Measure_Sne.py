8e5f30e1a8ff721d48e1b61c3d52065c218b7083
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-06-01 17:16:04 +0100

    First Test

--- Measure_Sne.py
+++ Measure_Sne.py
@@ -10,14 +10,18 @@ GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
 GPIO.setup(18, GPIO.OUT)# set GPIO 18 as output for supernova led  
 GPIO.setup(23, GPIO.OUT)# set GPIO 23 as output for green led  
 GPIO.setup(24, GPIO.OUT)# set GPIO 24 as CX1 for LED
+GPIO.setup(25, GPIO.OUT)#set 25 as static red LED
+GPIO.setup(17, GPIO.OUT)#set 17 as sin wave yellow
 
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #white = GPIO.PWM(24,10000)    # create object white for PWM on port 24 at 100 Hertz  
 red = GPIO.PWM(18, 400)      # create object red for PWM on port 23 at 100 Hertz  
-cx1 = GPIO.PWM(24, 400)  
+cx1 = GPIO.PWM(24, 400)
+sin = GPIO.PWM(17, 400)
 #white.start(0)              # start white led on 0 percent duty cycle (off)  
 red.start(0)              # red fully on (100%)  
-cx1.start(0)  
+cx1.start(0) 
+sin.start(0) 
 # now the fun starts, we'll vary the duty cycle to   
 # dim/brighten the leds, so one is bright while the other is dim  
   
@@ -25,6 +29,7 @@ pause_time = 0.1           # you can change this to slow down/speed up
 t=np.load('Ia_Time.npy')
 m=np.load('Ia_Mag.npy')
 cx=np.load('CX.npy')
+sinw=np.load('Sine_Wave.npy')
 
 tsl=TSL2561()
 
@@ -42,10 +47,12 @@ try:
 			print '--- Hubble Observation Started ----'
 			time_array=[]
 			luxarray=[]
+			GPIO.output(25, True)
 			for i in range(4,len(m),2):      # 101 because it stops when it finishes 100  
 			     
 			    red.ChangeDutyCycle(m[i])
 			    cx1.ChangeDutyCycle(cx[i])
+			    sinw.ChangeDutyCycle(sinw[i])
 			    l1=[]
 			    for j in range(0,5):
 			    	l1.append(tsl.readFull())
@@ -64,6 +71,8 @@ try:
 
 			red.ChangeDutyCycle(0)
 			cx1.ChangeDutyCycle(0)
+			sinw.ChangeDutyCycle(0)
+			GPIO.output(25, False)
 			plt.title('Object Lightcurve')
 			plt.tick_params(axis='y',which='both', left='off', right='off', labelleft='off')
 			plt.xlabel('Days since Supernova Explosion')
